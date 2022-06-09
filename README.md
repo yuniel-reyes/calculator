c997588b9b28c4e672b5557f48f6bb2fd7774964
Yuniel <yreyes8827@gmail.com>
2022-06-09 14:01:51 +0200

    Add basic functionalities and general UI design

--- README.md
+++ README.md
@@ -13,4 +13,19 @@ A calculator app using HTML, CSS and JS
 
 #### Bottleneck
 
+- How to evaluate expressions represented as strings?
+
+1. Create a function for each of the following basic math operations:
+    - Add
+    - Subtract
+    - Multiply
+    - Divide
+2.  Create a function called `operate()`
+    - The function should take as argument 2 numbers and 1 operator: `- + * / `
+    - Depending on the operator, the function should call one of the 
+      above functions.
+    * Solution: Let's start easy: create a prompt box for each number and and one for the operator
+3. Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key.
+                  
+
 
--- calculator.js
+++ calculator.js
@@ -1,1 +1,44 @@
-// 
+// add() function
+function add(a, b){
+    return alert(a + b);
+}
+
+// subtract() function
+function subtract(a, b){
+    return alert(a - b);
+}
+
+// multiply() function
+function multiply(a, b){
+    return alert(a * b);
+}
+
+// divide() function
+function divide(a, b){
+    return alert(a / b);
+}
+
+// operator() function
+// The function takes 3 arguments:
+// 2 numbers and 1 operator
+// Then it call one of the above function
+// depending on the operator
+function operator(){
+    let firstNumber = +prompt("Enter first number");
+    let theOperator = prompt("Enter operator");
+    let secondNumber = +prompt("Enter second number");
+    switch (theOperator) {
+        case '*':
+            return multiply(firstNumber, secondNumber)
+        case '/':
+            return divide(firstNumber, secondNumber);
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        default:
+            return alert("Operator has to be: + - * /");         
+    }
+}
+
+// operator();
