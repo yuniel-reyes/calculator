09e8e66c6e1ab8f8f0ce89224bcb116859964ffb
Yuniel <yreyes8827@gmail.com>
2022-06-15 11:48:17 +0200

    Refactoring and splitting functionalities Update README Add clear Add decimal point

--- README.md
+++ README.md
@@ -27,7 +27,10 @@ A calculator app using HTML, CSS and JS
     - The function should take as argument 2 numbers and 1 operator: `- + * / `
     - Depending on the operator, the function should call one of the 
       above functions.
-    - The function will update the calculation and the result area   
+    - The function will update the calculation and the result area
+    - If return value is floating point number, truncate the number
+      without rounding.  
+
 
 3. Create the functions that populate the display when number/ 
    operator buttons are pressed. Store the display value:   
@@ -37,35 +40,42 @@ A calculator app using HTML, CSS and JS
     - Add an event listener to each number/operator button       
     b) Call the function `populate()`. This function should: 
       - Get pressed button
+      **First Number**
       - Get and display the first number. The first number will be 
         the entered number until an operator button is pressed. Or 0 if
         any operator is pressed before any number. So:
-      - If not theOperator, pressed button not [−=×÷\+], not first number == 0
+      - If not operator, pressed button not [−=×÷\+], not first number == 0
         and pressed button not 0, call `getFirstNumber()` 
           `getFirstNumber()` will:
-          -  if not first number and pressed button from 1-9
-          -  if 0 is pressed
+          -  if not first number and pressed button from 1-9, FN = PB
+          -  if FN, FN = FN + PB
       - Display first number
-      - Get and display the operator (FN == 0 or not, but not SN):
-        If PB [+-/*=], call `getOperator()`
+      **Operator**
+      - Get and display the operator (FN == 0 or not, but not second number):
+        If PB [+-/*=] but not SN, call `getOperator()`
           `getOperator()` will:
-        - if FN = 0, store number   
+        - if not FN, FN = 0   
         - if theOperator is empty, store operator
         - if theOperator not empty, update `theOperator`
-      - Display operation in result display 
+      - Display operation in result display
+      **Second Number** 
       - Get and display second number. The second number will be the entered number
       if theOperator is not empty and any number button is pressed: 
         - If not SN: secondNumber = pressedNumber;
-        - If SN, SN += pressedNumber
+        - If SN, SN = SN + PB
       
       - Check result. This connects with 4.
-        - If [+-/*=] and SN, call operator()
-        - If not [+-/*]: show operation in RD, return calculation
-        - If [+-/*]: 
-            FN = result of calculation
-            SN = "";
-            theOperator = PB  
-             
+        - If [+-/*=] and SN , call `resultWithEqual()` or `resultWithOtherOperators()`
+        `resultWithEqual()` will:
+          - check that not division by 0
+          - return calculation
+        `resultWithOtherOperators()` will:
+          - get calculation
+          - update FN
+          - update SN
+          - update operator
+          - display current operation (calculation area and result area)
+
 4. Users should be able to string together several operation 
   and get the right answer, with each pair of numbers being 
   evaluate at a time:  
@@ -73,13 +83,6 @@ A calculator app using HTML, CSS and JS
                   19 * 3 =    
   - The calculator should not evaluate more than a single
       pair of numbers at a time
-
-  - If other operator is pressed and FN, SN, theOperator:
-    - get result: call `operator()`
-      firstNumber = result of operator()
-      secondNumber = "";
-      theOperator = pressedBnt;
-  - Round decimal numbers
 
 5. Clear button functionality
    If AC is pressed the calculator should be reseted:
@@ -92,9 +95,22 @@ A calculator app using HTML, CSS and JS
   - Get reference of the point button
   - Add event handler to it
   - If user presses the decimal point button, and not FN or SN was entered,
-    the program should assume a 0 in the integer part
+    the program should assume a 0 in the integer part. 
+  - DPB is pressed:
+    First number
+    - If not operator, is first number
+    - If not first number, then 0.
+    - If first number but not operator then FN + .
+
+    Second number
+    - If operator and first number, but not second number, SN = 0.
+    - Else, SN = SN + .
+
   - If user presses decimal point button:  
     - call `getFirstNumber()`/`getSecondNumber()`
+
+8. Create global variables to store the first number, the second number,
+   and the operator
        
 
 
--- calculator.js
+++ calculator.js
@@ -35,14 +35,18 @@ function operator(firstNumber, secondNumber, theOperator){
             if (Number.isInteger(theMultiplication)){
                 return theMultiplication;
             } else {
-                return theMultiplication.toFixed(1);
+                // truncate the number to 2 decimal points without rounding
+                theMultiplication = theMultiplication.toString().slice(0, (theMultiplication.toString().indexOf(".") + 3));
+                return Number(theMultiplication);
             }
         case '÷':
             let theDivision = divide(thisFirstNumber, thisSecondNumber);
             if (Number.isInteger(theDivision)){
                 return theDivision;
             } else {
-                return theDivision.toFixed(1); 
+                // truncate the number to 2 decimal points without rounding
+                theDivision =  theDivision.toString().slice(0, (theDivision.toString().indexOf(".") + 3));
+                return Number(theDivision); 
             }
         case '+':
             let theSum = add(thisFirstNumber, thisSecondNumber);
@@ -58,12 +62,12 @@ function operator(firstNumber, secondNumber, theOperator){
 // The getFirstNumber function check for the firstNumber 
 function getFirstNumber(thisBtn){
     let thisNumber = thisBtn;
-    // do not repeat 0 and do not enter . as first char
+    // not first number
     if (firstNumber.length == 0 && thisNumber.match(/[1-9]/)){
         // Store firstNumber
         firstNumber = thisNumber;
         return firstNumber
-    } else if (Boolean(firstNumber)) { 
+    } else if (firstNumber) { 
         // firstNumber is of two or more digits
         firstNumber += thisNumber;
         return firstNumber;  
@@ -90,10 +94,6 @@ function getOperator(thisBtn){
         theOperator = thisBtn;
         return theOperator;
     }
-
-    // If operator, first and second number already exist
-    // call operation
-
 } 
 
 
@@ -101,12 +101,66 @@ function getOperator(thisBtn){
 function getSecondNumber(thisBtn){
     let thisNumber = thisBtn;
 
-    if (secondNumber.length == 0){
+    if (secondNumber.length == 0 || secondNumber === "0"){
         secondNumber = thisNumber;
     } else {
         secondNumber += thisNumber;
     }
     return secondNumber;
+}
+
+// Create decimalPoint()
+function decimalPoint(e){
+    theDecimalPoint = e.target.textContent;
+    
+    // Check that there is not decimal point
+    if (calculationArea.textContent.includes(".")){
+        return;
+    }
+
+    // first number
+    // not first number = 0.
+    if (!theOperator && !firstNumber){
+        firstNumber = 0 + theDecimalPoint;
+        return calculationArea.textContent = firstNumber;
+    }
+    if (!theOperator && firstNumber) {
+        firstNumber = firstNumber + theDecimalPoint;
+        return calculationArea.textContent = firstNumber;
+    }
+    // second number = 0.
+    if ((theOperator && firstNumber) && !secondNumber){
+        secondNumber = 0 + theDecimalPoint;
+        return calculationArea.textContent = secondNumber;
+    } else {
+        secondNumber = secondNumber + theDecimalPoint;
+        return calculationArea.textContent = secondNumber;
+    }
+    
+}
+
+// Create resultWithEqual()
+function resultWithEqual(){
+
+    // check that calculation is not division by 0
+    if (secondNumber === "0" && theOperator == "÷"){
+        return alert("You can't divide by 0!");
+    } else {
+        let thisOperation = operator(firstNumber, secondNumber, theOperator);
+        calculationArea.textContent = thisOperation;
+        return theResultDisplay.textContent = theResultDisplay.textContent + " " + secondNumber + " =";   
+    }
+}
+
+// Create resultWithOthersOperators()
+function resultWithOthersOperators(pressedBtn){    
+    let thisOperation = operator(firstNumber, secondNumber, theOperator);
+    firstNumber = thisOperation;
+    secondNumber = "";
+    theOperator = pressedBtn;
+    theResultDisplay.textContent = firstNumber + " " + theOperator;
+    calculationArea.textContent = firstNumber;
+    return; 
 }
 
 
@@ -116,7 +170,8 @@ function populate(e){
     let pressedBtn = e.target.textContent;
     console.log(pressedBtn);
 
-    // The getFirstNumber function will be called if not operator 
+    // The getFirstNumber function will be called if not operator
+    // not operator, PB not operator and not first number being 0 
     if (theOperator.length == 0 && !pressedBtn.match(/[\−=×÷\+]/)
         && !(firstNumber.length == 0 && pressedBtn == "0")){
         getFirstNumber(pressedBtn);
@@ -130,26 +185,18 @@ function populate(e){
     // Get second number
     if (theOperator.length !== 0 && pressedBtn.match(/[0-9]/)){
             getSecondNumber(pressedBtn);
-            if (secondNumber === "0" && theOperator == "÷"){
-                secondNumber = "";
-                return alert("You can't divide by 0!");
-            } else {
             return calculationArea.textContent = secondNumber;    
-        }
     }
+
     // Check result
     if (pressedBtn.match(/[−=\×÷\+]/) && secondNumber){
         if (pressedBtn == "="){
-            let thisOperation = operator(firstNumber, secondNumber, theOperator);
-            calculationArea.textContent = thisOperation;
-            theResultDisplay.textContent = theResultDisplay.textContent + " " + secondNumber + " =";   
+            // call resultWithEqual
+            return resultWithEqual();
         } else {
-            let thisOperation = operator(firstNumber, secondNumber, theOperator);
-            firstNumber = thisOperation;
-            secondNumber = "";
-            theOperator = pressedBtn;
-            theResultDisplay.textContent = firstNumber + " " + theOperator;
-            calculationArea.textContent = firstNumber; 
+            // call resultWithOthersOperators()
+            resultWithOthersOperators(pressedBtn);
+            return;
         }
     }
 }
@@ -197,6 +244,6 @@ theResetBtn.addEventListener('click', reset);
 // Get reference of decimal point button
 const decimalBtn = document.getElementById('bullet');
 // Add event handler to the decimal point button
-decimalBtn.addEventListener('click', populate);
+decimalBtn.addEventListener('click', decimalPoint);
 
 
